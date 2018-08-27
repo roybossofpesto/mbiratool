a7b4ffe7066d339853301a4941fd1a7648a89961
Pierre Gueth <pierre.gueth@gmail.com>
2018-08-27 23:16:19 +0200

    basic mbira

--- main.js
+++ main.js
@@ -15,18 +15,87 @@ const tunings = [
 ]
 
 $(document).ready(() => {
-    const dial_size = 360;
+    const base_size = 360;
     $('div.dial').each(function() {
-        const paper = Raphael(this, dial_size, dial_size);
-        paper.circle(dial_size / 2, dial_size / 2, dial_size / 2 - 10).attr({
+        const paper = Raphael(this, base_size, base_size);
+        paper.circle(base_size / 2, base_size / 2, base_size / 2 - 10).attr({
             'fill': '#f0f',
             'stroke-width': 20,
             'stroke': '#0ff',
         });
     })
 
-    // demo page http://anthonyterrien.com/demo/knob/
-    $('.knob').knob();
+    $('div.mbira').each(function() {
+        const pen_width = 2;
+        const keys = [0, 1, 2, 3, 4, 5, 6];
+        const paper = Raphael(this, base_size, base_size);
+
+        paper.rect(pen_width, pen_width, base_size - 2 * pen_width, base_size - 2 * pen_width, 10).attr({
+            "stroke-width": 2 * pen_width,
+            "fill": "#eee"
+        });
+        paper.rect(20, 40, base_size - 40, base_size - 80, 0).attr({
+            "stroke-width": 2 * pen_width,
+            "fill": "#777"
+        });
+        paper.circle(base_size - 55, base_size - 75, 20).attr({
+            "stroke-width": 2 * pen_width,
+            "fill": "#fff"
+        });
+
+        const key_shape = (width, height, key) => {
+            const key_third = (key + 2) % keys.length;
+            const key_fifth = (key + 4) % keys.length;
+            const key_colors = ["#56b0fd", "#fe6e32", "#aee742", "#b75ac4", "#fff436", "#40534f", "#ffc0d1"];
+            const path_string = "M0,0l0," + height + "c0," + width + "," + width + "," + width + "," + width + ",0l0," + (-height) + "z";
+            const path_object = paper.path(path_string);
+            path_object.attr({
+                "stroke-width": 2 * pen_width,
+                "fill": key_colors[key],
+            });
+            path_object.key = key;
+            path_object.hover(function() {
+                console.log('in', this.key + 1)
+                // for (let kk = 0; kk < keys.length; kk++) {
+                //     if (kk == key) continue;
+                //     if (kk == key_third) continue;
+                //     if (kk == key_fifth) continue;
+                //     for (var ll = 0; ll < keys[kk].length; ll++) keys[kk][ll].attr("fill", "#fff");
+                // }
+                // document.body.style.cursor = "pointer";
+            }, function() {
+                console.log('out', this.key + 1)
+                // document.body.style.cursor = "auto";
+                // for (let kk = 0; kk < keys.length; kk++) {
+                //     for (var ll = 0; ll < keys[kk].length; ll++) keys[kk][ll].attr("fill", key_colors[kk]);
+                // }
+            });
+            return path_object;
+        };
+
+        const big_key_width = 24;
+        const small_key_width = 15.35;
+        const key_offset = 15;
+        const lower_left_keys = [1, 6, 5, 4, 3, 2, 0];
+        const upper_left_keys = [1, 0, 6, 5, 3, 4, 0];
+        const right_keys = [0, 1, 2, 3, 4, 5, 6, 0, 1];
+        lower_left_keys.forEach((key, kk) => {
+            key_shape(big_key_width, 210 + 10 * kk, key).translate(key_offset + big_key_width * (kk + .5), key_offset);
+        })
+        upper_left_keys.forEach((key, kk) => {
+            key_shape(big_key_width, 150 + 8 * kk, key).translate(key_offset + big_key_width * kk, key_offset);
+        })
+        key_shape(big_key_width, 170, 2).translate(key_offset + big_key_width * upper_left_keys.length, key_offset);
+        right_keys.forEach((key, kk) => {
+            key_shape(small_key_width, 150 - 5 * kk, key).translate(base_size - key_offset + small_key_width * (kk - right_keys.length), key_offset);
+        })
+
+        paper.rect(pen_width, 35, base_size - 2 * pen_width, 20, 0).attr({
+            "stroke-width": 2 * pen_width,
+            "fill": "#777"
+        });
+
+    })
 
     const first_song_blocks = $('#first-song>div>span');
     const second_song_blocks = $('#second-song>div>span');
@@ -55,6 +124,8 @@ $(document).ready(() => {
     let current_tuning = parseInt($('#tuning-slider').val());
     let current_mode = parseInt($('#mode-slider').val());
     let use_letter_alphabet = $('#letters-checkbox').prop('checked');
+
+    // knob demo page http://anthonyterrien.com/demo/knob/
     $('#tuning-slider').knob({
         'width': 100,
         'height': 100,
