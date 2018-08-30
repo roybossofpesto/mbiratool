087cd77755b3abc921f45b80285b641697975350
roybossofpesto <andreduracell@gmail.com>
2018-08-30 22:22:14 +0200

    update mbira with tuning

--- main.js
+++ main.js
@@ -42,7 +42,10 @@ $(document).ready(() => {
         })
     })
 
-    $('div.mbira').each(function() {
+    let tuning = parseInt($('#tuning-knob').val());
+    let mode = parseInt($('#mode-knob').val());
+    let use_letter_alphabet = $('#letters-checkbox').prop('checked');
+    const update_mbiras = $('div.mbira').map(function() {
         const pen_width = 2;
         const paper = Raphael(this, base_size, base_size);
 
@@ -59,6 +62,11 @@ $(document).ready(() => {
             "fill": "#fff"
         });
 
+        const reset_colors = () => {
+            keys.forEach((foo) => {
+                foo.attr('fill', root_key_colors[wrap(foo.key + tuning)])
+            })
+        };
         const keys = [];
         const create_key = (width, height, key) => {
             const key_third = (key + 2) % 7;
@@ -81,11 +89,7 @@ $(document).ready(() => {
                         "white";
                     foo.attr('fill', color);
                 })
-            }, () => {
-                keys.forEach((foo) => {
-                    foo.attr('fill', root_key_colors[foo.key])
-                })
-            });
+            }, reset_colors);
             return path_object;
         };
 
@@ -110,14 +114,13 @@ $(document).ready(() => {
             "stroke-width": 2 * pen_width,
             "fill": "#777"
         });
-    })
+
+        return reset_colors;
+    }).get();
 
     const first_song_blocks = $('#first-song>div>span');
     const second_song_blocks = $('#second-song>div>span');
     const third_song_blocks = $('#third-song>div>span');
-    let tuning = parseInt($('#tuning-knob').val());
-    let mode = parseInt($('#mode-knob').val());
-    let use_letter_alphabet = $('#letters-checkbox').prop('checked');
     const update_songs = () => {
         const alphabet = use_letter_alphabet ? letters : numbers;
         const value = mode + use_letter_alphabet * tuning;
@@ -151,6 +154,11 @@ $(document).ready(() => {
         })
     };
 
+    const update_all = () => {
+        update_songs();
+        update_mbiras.forEach((foo) => foo());
+    }
+
     // knob demo page http://anthonyterrien.com/demo/knob/
     $('#tuning-knob').knob({
         'width': 100,
@@ -164,7 +172,7 @@ $(document).ready(() => {
         'format': (foo) => {
             tuning = foo
             $('div.view.tuning').text(tunings[wrap(tuning)]);
-            update_songs();
+            update_all();
             return foo;
         },
     })
@@ -180,13 +188,13 @@ $(document).ready(() => {
         'format': (foo) => {
             mode = foo
             $('div.view.mode').text(numbers[wrap(mode)]);
-            update_songs();
+            update_all();
             return foo;
         },
     })
     $('#letters-checkbox').on('change', function() {
         use_letter_alphabet = this.checked;
-        update_songs();
+        update_all();
     });
 
 })
