3ac3aaed75ebe7174aed4187e77eb0fd44eae776
Pierre Gueth <pierre.gueth@gmail.com>
2018-09-09 04:40:32 +0200

    dial highlight key

--- main.js
+++ main.js
@@ -69,6 +69,85 @@ $(document).ready(() => {
     let mode = parseInt($('#mode-knob').val());
     let use_letter_alphabet = $('#letters-checkbox').prop('checked');
 
+    const update_mbiras = $('div.mbira').map(function() {
+        const pen_width = 2;
+        const paper = Raphael(this, base_size, base_size);
+
+        paper.rect(pen_width, pen_width, base_size - 2 * pen_width, base_size - 2 * pen_width, 10).attr({
+            "stroke-width": 2 * pen_width,
+            "fill": "#eee"
+        });
+        paper.rect(20, 40, base_size - 40, base_size - 80, 0).attr({
+            "stroke-width": 2 * pen_width,
+            "fill": "#777"
+        });
+        paper.circle(base_size - 55, base_size - 75, 20).attr({
+            "stroke-width": 2 * pen_width,
+            "fill": "#fff"
+        });
+
+        const reset_colors = () => {
+            keys.forEach((foo) => {
+                foo.attr('fill', root_key_colors[wrap(foo.key + tuning)])
+            })
+        };
+        const highlight_colors = function() {
+            const key = this.key
+            const key_third = (key + 2) % 7;
+            const key_fifth = (key + 4) % 7;
+            keys.forEach((foo, kk) => {
+                const base_color = root_key_colors[wrap(key + tuning)];
+                const color =
+                    foo.key == key ? base_color :
+                    foo.key == key_third ? base_color.brighten(2.5) :
+                    foo.key == key_fifth ? base_color.brighten(1) :
+                    "white";
+                foo.attr('fill', color);
+            })
+
+        };
+        const keys = [];
+        const create_key = (width, height, key) => {
+            const path_string = "M0,0l0," + height + "c0," + width + "," + width + "," + width + "," + width + ",0l0," + (-height) + "z";
+            const path_object = paper.path(path_string);
+            path_object.attr({
+                "stroke-width": 2 * pen_width,
+                "fill": root_key_colors[wrap(key + tuning)],
+            });
+            path_object.key = key;
+            keys.push(path_object);
+            path_object.hover(highlight_colors, reset_colors);
+            return path_object;
+        };
+
+        const big_key_width = 24;
+        const small_key_width = 15.35;
+        const key_offset = 15;
+        const lower_left_keys = [1, 6, 5, 4, 3, 2, 0];
+        const upper_left_keys = [1, 0, 6, 5, 3, 4, 0];
+        const right_keys = [0, 1, 2, 3, 4, 5, 6, 0, 1];
+        lower_left_keys.forEach((key, kk) => {
+            create_key(big_key_width, 210 + 10 * kk, key).translate(key_offset + big_key_width * (kk + .5), key_offset);
+        })
+        upper_left_keys.forEach((key, kk) => {
+            create_key(big_key_width, 150 + 8 * kk, key).translate(key_offset + big_key_width * kk, key_offset);
+        })
+        create_key(big_key_width, 170, 2).translate(key_offset + big_key_width * upper_left_keys.length, key_offset);
+        right_keys.forEach((key, kk) => {
+            create_key(small_key_width, 150 - 5 * kk, key).translate(base_size - key_offset + small_key_width * (kk - right_keys.length), key_offset);
+        })
+
+        paper.rect(pen_width, 35, base_size - 2 * pen_width, 20, 0).attr({
+            "stroke-width": 2 * pen_width,
+            "fill": "#777"
+        });
+
+        return {
+            highlight: highlight_colors,
+            reset: reset_colors,
+        };
+    }).get();
+
     const update_dials = $('div.dial').map(function() {
         const paper = Raphael(this, base_size, base_size);
         paper.customAttributes.arc = function(centerX, centerY, startAngle, endAngle, innerR, outerR) {
@@ -105,8 +184,8 @@ $(document).ready(() => {
         });
 
         const sectors = []
-        for (let kk = 0; kk < 48; kk++)
-            sectors.push(paper
+        for (let kk = 0; kk < 48; kk++) {
+            const sector = paper
                 .path()
                 .attr({
                     "stroke-width": 0,
@@ -114,8 +193,14 @@ $(document).ready(() => {
                     'fill': root_key_colors[Math.floor(kk / 4) % 7],
                     'arc': [base_size / 2, base_size / 2, 0, 360 / 48 + .5, 50, 160],
                 })
-                .rotate(360 * kk / 48, base_size / 2, base_size / 2))
-
+                .rotate(360 * kk / 48, base_size / 2, base_size / 2);
+            sector.hover(function() {
+                update_mbiras.forEach((foo) => foo.highlight.call({
+                    key: this.chord - tuning,
+                }));
+            }, () => update_mbiras.forEach((foo) => foo.reset()));
+            sectors.push(sector);
+        }
 
         const update = () => {
             const chords = [];
@@ -126,85 +211,13 @@ $(document).ready(() => {
                 const cc = wrap(value + 4 + (index > 0));
                 chords.push(aa, aa, aa, aa, bb, bb, bb, bb, cc, cc, cc, cc)
             })
-            console.log('update dial', chords, sectors)
             sectors.forEach((sector, index) => {
-                sector.attr('fill', root_key_colors[chords[index]].brighten(index % 2 == 0 ? 0 : 1))
+                const chord = chords[index];
+                sector.chord = chord;
+                sector.attr('fill', root_key_colors[chord].brighten(index % 2 == 0 ? 0 : 1))
             })
         }
         return update;
-    }).get();
-
-    const update_mbiras = $('div.mbira').map(function() {
-        const pen_width = 2;
-        const paper = Raphael(this, base_size, base_size);
-
-        paper.rect(pen_width, pen_width, base_size - 2 * pen_width, base_size - 2 * pen_width, 10).attr({
-            "stroke-width": 2 * pen_width,
-            "fill": "#eee"
-        });
-        paper.rect(20, 40, base_size - 40, base_size - 80, 0).attr({
-            "stroke-width": 2 * pen_width,
-            "fill": "#777"
-        });
-        paper.circle(base_size - 55, base_size - 75, 20).attr({
-            "stroke-width": 2 * pen_width,
-            "fill": "#fff"
-        });
-
-        const reset_colors = () => {
-            keys.forEach((foo) => {
-                foo.attr('fill', root_key_colors[wrap(foo.key + tuning)])
-            })
-        };
-        const keys = [];
-        const create_key = (width, height, key) => {
-            const key_third = (key + 2) % 7;
-            const key_fifth = (key + 4) % 7;
-            const path_string = "M0,0l0," + height + "c0," + width + "," + width + "," + width + "," + width + ",0l0," + (-height) + "z";
-            const path_object = paper.path(path_string);
-            path_object.attr({
-                "stroke-width": 2 * pen_width,
-                "fill": root_key_colors[wrap(key + tuning)],
-            });
-            path_object.key = key;
-            keys.push(path_object);
-            path_object.hover(() => {
-                keys.forEach((foo, kk) => {
-                    const base_color = root_key_colors[wrap(key + tuning)];
-                    const color =
-                        foo.key == key ? base_color :
-                        foo.key == key_third ? base_color.brighten(2.5) :
-                        foo.key == key_fifth ? base_color.brighten(1) :
-                        "white";
-                    foo.attr('fill', color);
-                })
-            }, reset_colors);
-            return path_object;
-        };
-
-        const big_key_width = 24;
-        const small_key_width = 15.35;
-        const key_offset = 15;
-        const lower_left_keys = [1, 6, 5, 4, 3, 2, 0];
-        const upper_left_keys = [1, 0, 6, 5, 3, 4, 0];
-        const right_keys = [0, 1, 2, 3, 4, 5, 6, 0, 1];
-        lower_left_keys.forEach((key, kk) => {
-            create_key(big_key_width, 210 + 10 * kk, key).translate(key_offset + big_key_width * (kk + .5), key_offset);
-        })
-        upper_left_keys.forEach((key, kk) => {
-            create_key(big_key_width, 150 + 8 * kk, key).translate(key_offset + big_key_width * kk, key_offset);
-        })
-        create_key(big_key_width, 170, 2).translate(key_offset + big_key_width * upper_left_keys.length, key_offset);
-        right_keys.forEach((key, kk) => {
-            create_key(small_key_width, 150 - 5 * kk, key).translate(base_size - key_offset + small_key_width * (kk - right_keys.length), key_offset);
-        })
-
-        paper.rect(pen_width, 35, base_size - 2 * pen_width, 20, 0).attr({
-            "stroke-width": 2 * pen_width,
-            "fill": "#777"
-        });
-
-        return reset_colors;
     }).get();
 
     const first_song_blocks = $('#first-song>div>span');
@@ -245,7 +258,7 @@ $(document).ready(() => {
 
     const update_all = () => {
         update_songs();
-        update_mbiras.forEach((foo) => foo());
+        update_mbiras.forEach((foo) => foo.reset());
         update_dials.forEach((foo) => foo());
     }
 
