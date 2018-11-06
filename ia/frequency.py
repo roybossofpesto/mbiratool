a03d6f0d65eec94ec3a4ca1f04e6d9595ec4a236
Pierre Gueth <pierre.gueth@gmail.com>
2018-11-06 01:42:12 +0100

    sample geenration

--- ia/frequency.py
+++ ia/frequency.py
@@ -3,31 +3,36 @@
 
 from pylab import *
 
-ts = arange(0, 5e-2, 1./44100, dtype=float)
-freqs = logspace(1.7, 4, 5)
-factors = 1 + .2 * (rand(3) - .5)
-factors[0] = 1
+ts = arange(0, 25e-3, 1./44100, dtype=float)
+freqs = logspace(2.5, 4, 5)
+#freqs = array([400])
 print("time", ts.shape)
-print("freqs/factors", freqs.shape, factors.shape)
+print("freqs", freqs.shape)
 
-data = []
-labels = []
+def serie(freq, factor=None, phase=None):
+    if factor is None: factor = 1 + .1 * (rand() - .5)
+    if phase is None: phase = 2j * pi * rand()
+    freq_ = factor * freq
+    noise = .2 * randn(ts.shape[0])
+    ys = exp(2j * pi * freq_ * ts - phase) + noise
+    return abs(fft(ys, 512))
+    #return abs(fft(ys, 512)[256:][::-1])
+
+nn = 10
+samples = []
+targets = []
 for ident, freq in enumerate(freqs):
-    labels.append(ident * ones(factors.shape[0]))
-    for factor in factors:
-        freq_ = factor * freq
-        serie = cos(2 * pi * freq_ * ts)+.2*randn(ts.shape[0])
-        data.append(abs(fft(serie, 512)[:256]))
+    targets.append(ident)
+    foo = [serie(freq, 1)]
+    for kk in range(nn - 1):
+        foo.append(serie(freq))
+    samples.append(foo)
 
-data = array(data)
-labels = hstack(labels)
-print("data", data.shape)
-print("labels", labels.shape)
 
-figure()
-for serie in data:
-    print(serie.shape)
-    plot(serie)
+print("samples", len(samples))
+print("targets", len(targets))
+
+show()
 
 import torch
 import torch.nn as nn
@@ -37,13 +42,13 @@ import torch.nn.functional as F
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        self.conv1 = nn.Conv1d(2, 1, 16)
+        self.conv1 = nn.Conv1d(1, 1, 16)
         self.fc1 = nn.Linear(16, 64)
         self.fc2 = nn.Linear(64, 10)
     def forward(self, x):
         x = F.relu(self.conv1(x))
         x = F.max_pool1d(x, 2)
-        print('foobar', x.size())
+        #print('foobar', x.size())
         x = F.relu(self.fc1(x))
         x = self.fc2(x)
         return x
@@ -58,8 +63,12 @@ optimizer = optim.SGD(net.parameters(), lr=0.01, momentum=.9)
 #criterion = nn.CrossEntropyLoss()
 criterion = nn.MSELoss()
 
-sample = torch.randn(1, 2, 47)
-target = torch.randn(1, 1, 10)
+for target, sample in zip(targets, samples):
+    sample = torch.tensor(sample).unsqueeze(1)
+    print("training", target, sample.shape)
+
+sample = torch.randn(10, 1, 47)
+target = torch.randn(10, 1, 10)
 
 optimizer.zero_grad()
 prediction = net(sample)
@@ -68,7 +77,6 @@ loss = criterion(prediction, target)
 print(loss, loss.size())
 loss.backward()
 optimizer.step()
-
 
 show()
 
