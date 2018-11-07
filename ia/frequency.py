b049ead17bf290c84372fbe03989f67b50ee7db3
Pierre Gueth <pierre.gueth@gmail.com>
2018-11-07 02:12:44 +0100

    kind of work

--- ia/frequency.py
+++ ia/frequency.py
@@ -1,6 +1,20 @@
 #!/usr/bin/env python3
 # coding: utf-8
 
+import argparse
+
+parser = argparse.ArgumentParser(description='mbira autochord.')
+parser.add_argument('--nepoch', metavar='N', type=int, default=4,
+                    help='number of training epoch')
+parser.add_argument('--ntraining', metavar='M', type=int, default=25,
+                    help='training set size')
+parser.add_argument('--sum', dest='accumulate', action='store_const',
+                    const=sum, default=max,
+                    help='sum the integers (default: find the max)')
+
+args = parser.parse_args()
+print(args)
+
 from pylab import *
 
 ts = arange(0, 25e-3, 1./44100, dtype=float)
@@ -18,13 +32,12 @@ def serie(freq, factor=None, phase=None):
     return abs(fft(ys, 256))
     #return abs(fft(ys, 512)[256:][::-1])
 
-nn = 1000
 samples = []
 targets = []
 for ident, freq in enumerate(freqs):
     targets.append(ident)
     foo = [serie(freq, 1)]
-    for kk in range(nn - 1):
+    for kk in range(args.ntraining - 1):
         foo.append(serie(freq))
     samples.append(foo)
 
@@ -42,12 +55,14 @@ class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
         self.conv1 = nn.Conv1d(1, 1, 128)
-        self.fc1 = nn.Linear(64, 64)
-        self.fc2 = nn.Linear(64, 7)
+        #self.pool = nn.MaxPool1d(2)
+        self.fc1 = nn.Linear(129, 64)
+        self.fc2 = nn.Linear(64, 10)
     def forward(self, x):
         x = F.relu(self.conv1(x))
-        x = F.max_pool1d(x, 2)
         #print('forward', x.size())
+        x = x.view(-1, 129)
+        #x = self.pool(x, 2)
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
         return x
@@ -59,41 +74,48 @@ print("params", len(params))
 print("input_shape", params[0].size())
 
 optimizer = optim.SGD(net.parameters(), lr=0.01, momentum=.9)
-#criterion = nn.CrossEntropyLoss()
-criterion = nn.MSELoss()
+criterion = nn.CrossEntropyLoss()
+#criterion = nn.MSELoss()
 
 losses = []
-for epoch in range(6):
+for epoch in range(args.nepoch):
+    print("-", epoch, end=" ")
+    loss_accum = 0
     for index, sample in zip(targets, samples):
         sample = torch.tensor(sample, dtype=torch.float).unsqueeze(1)
-        target = torch.zeros(len(sample), 1, 7)
-        target[:, :, index] = 1
-        print("training", target.shape, sample.shape)
+        target = torch.zeros(sample.shape[0], dtype=torch.long)
+        target[:] = index
+        #print("training", target.shape, sample.shape)
+        #print(target)
 
         optimizer.zero_grad()
         prediction = net(sample)
-        print("prediction", prediction.size())
+        #print("prediction", prediction.size())
+        #print(prediction)
 
         loss = criterion(prediction, target)
-        print("loss", loss)
-        losses.append(loss)
+        loss_accum += loss.item()
+        print(loss.item(), end=" ")
 
         loss.backward()
         optimizer.step()
+    print('--', loss_accum)
+    losses.append(loss_accum)
 
 figure()
 plot(losses)
 
 for index, freq in enumerate(freqs):
-    print("----", index, freq)
-    for kk in range(20):
+    print("#", index, freq, end=' ')
+    accum = 0
+    for kk in range(100):
         tensor = torch.from_numpy(serie(freq)).type(torch.float).unsqueeze(0).unsqueeze(1)
         prediction = net(tensor)
-        print(tensor.shape, prediction.shape, prediction.max(2), prediction.min())
-        print(prediction)
-        confidence, index_ = prediction.max(2)
-        #print(confidence, index_)
+        #print(tensor.shape, prediction.shape, prediction.min())
+        #print(prediction)
+        confidence, index_ = prediction.max(1)
+        #print(confidence.item(), index_.item())
+        accum += index == index_
+    print(accum.item())
 
 show()
-
-
