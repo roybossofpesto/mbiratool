d4b225d46584b14f7f94e9571dbb3a080dc60c14
Pierre Gueth <pierre.gueth@gmail.com>
2018-11-07 03:24:47 +0100

    definitely works

--- ia/frequency.py
+++ ia/frequency.py
@@ -1,27 +1,7 @@
 #!/usr/bin/env python3
 # coding: utf-8
 
-import argparse
-
-parser = argparse.ArgumentParser(description='mbira autochord.')
-parser.add_argument('--nepoch', metavar='N', type=int, default=4,
-                    help='number of training epoch')
-parser.add_argument('--ntraining', metavar='M', type=int, default=25,
-                    help='training set size')
-parser.add_argument('--sum', dest='accumulate', action='store_const',
-                    const=sum, default=max,
-                    help='sum the integers (default: find the max)')
-
-args = parser.parse_args()
-print(args)
-
 from pylab import *
-
-ts = arange(0, 25e-3, 1./44100, dtype=float)
-freqs = logspace(2.5, 4, 5)
-#freqs = array([400])
-print("time", ts.shape)
-print("freqs", freqs.shape)
 
 def serie(freq, factor=None, phase=None):
     if factor is None: factor = 1 + .1 * (rand() - .5)
@@ -31,6 +11,31 @@ def serie(freq, factor=None, phase=None):
     ys = exp(2j * pi * freq_ * ts - phase) + noise
     return abs(fft(ys, 256))
     #return abs(fft(ys, 512)[256:][::-1])
+
+import argparse
+
+parser = argparse.ArgumentParser(description='mbira autochord.')
+parser.add_argument('--nepoch', metavar='E', type=int, default=4,
+                    help='number of training epoch')
+parser.add_argument('--ntraining', metavar='M', type=int, default=25,
+                    help='training set size')
+parser.add_argument('--nclass', metavar='C', type=int, default=5,
+                    help='number of training class')
+parser.add_argument('--ntest', metavar='N', type=int, default=2000,
+                    help='test set size')
+parser.add_argument('--sum', dest='accumulate', action='store_const',
+                    const=sum, default=max,
+                    help='sum the integers (default: find the max)')
+
+ts = arange(0, 25e-3, 1./44100, dtype=float)
+freqs = logspace(2.5, 4, 5)
+#freqs = array([400])
+print("time", ts.shape)
+print("freqs", freqs.shape)
+
+args = parser.parse_args()
+args.nclass = max(args.nclass, freqs.shape[0])
+print(args)
 
 samples = []
 targets = []
@@ -57,7 +62,7 @@ class Net(nn.Module):
         self.conv1 = nn.Conv1d(1, 1, 128)
         #self.pool = nn.MaxPool1d(2)
         self.fc1 = nn.Linear(129, 64)
-        self.fc2 = nn.Linear(64, freqs.shape[0])
+        self.fc2 = nn.Linear(64, args.nclass)
     def forward(self, x):
         x = F.relu(self.conv1(x))
         #print('forward', x.size())
@@ -107,17 +112,25 @@ ylabel("loss")
 xlabel("epoch")
 semilogy(losses)
 
+corr = []
 for index, freq in enumerate(freqs):
     print("#", index, freq, end=' ')
-    accum = 0
-    for kk in range(100):
+    accum = torch.zeros(args.nclass)
+    for kk in range(args.ntest):
         tensor = torch.from_numpy(serie(freq)).type(torch.float).unsqueeze(0).unsqueeze(1)
         prediction = net(tensor)
         #print(tensor.shape, prediction.shape, prediction.min())
         #print(prediction.shape)
         confidence, index_ = prediction.max(1)
         #print(confidence.item(), index_.item())
-        accum += index == index_
-    print(accum.item())
+        accum[index_] += 1
+    accum = 100*accum/args.ntest
+    corr.append(accum)
+    print(accum)
+corr = torch.stack(corr).type(torch.int).numpy()
+perfect = np.all(corr == 100 * eye(corr.shape[0], corr.shape[1], dtype=int))
+
+print(corr, corr.shape)
+print("PERFECT !!!!!!" if perfect else ":(")
 
 show()
