8188c9c714475a3fd15d05699b3ae3a84ed4341f
Pierre Gueth <pierre.gueth@gmail.com>
2018-11-23 02:52:09 +0100

    more args

--- ia/mbira_export_wav.py
+++ ia/mbira_export_wav.py
@@ -1,16 +1,25 @@
 #!/usr/bin/env python3
 # coding: utf-8
 
+import argparse
+
+parser = argparse.ArgumentParser(description='mbira autochord recorder.')
+parser.add_argument('input', metavar='mbira.series', default="mbira.series",
+                    help='output series')
+
+args = parser.parse_args()
+
 import torch
 import scipy.io.wavfile as wav
 import numpy as np
 
-data = torch.load("mbira.series")
+print('loading', args.input)
+data = torch.load(args.input)
 notes = data["notes"]
 
 print(notes.keys(), len(notes))
 
 for note, series in notes.items():
-    filename = "note%d.wav" % note
+    filename = "note%d.wav" % (note + 1)
     wav.write(filename, 44100, (np.hstack(series) * 32768).astype(np.int16))
     print("wrote", filename)
--- ia/mbira_record.py
+++ ia/mbira_record.py
@@ -8,8 +8,12 @@ parser.add_argument('--output', metavar='mbira.series', default="mbira.series",
                     help='output series')
 parser.add_argument('--ntraining', metavar='M', type=int, default=200,
                     help='training set size. minimum number of chunk per class.')
+parser.add_argument('--nchunk', type=int, default=18,
+                    help='number of consecutive chunk above threshold.')
 parser.add_argument('--nnote', metavar='C_out', type=int, default=5,
                     help='number of output class. number of note')
+parser.add_argument('--threshold', type=float, default=.1,
+                    help='standard deviation threshold')
 parser.add_argument('--batch', type=bool, default=False,
                     help='batch mode')
 
@@ -28,16 +32,15 @@ chunk_size = stream.setperiodsize(1024)
 print(chunk_size)
 stream.dumpinfo()
 
-threshold = .1
 
 if not args.batch:
     plt.figure()
     line, = plt.plot(np.zeros(chunk_size))
     level = plt.axhline(0)
-    plt.axhline(threshold, color="r")
+    plt.axhline(args.threshold, color="r")
     plt.ylim(-1, 1)
 
-def get_series(nserie, nchunk=20):
+def get_series(nserie, nchunk):
     series = []
     chunks = []
     while True:
@@ -48,16 +51,16 @@ def get_series(nserie, nchunk=20):
             if not args.batch:
                 level.set_ydata(foo)
                 line.set_ydata(chunk)
-            if foo > threshold:
+            if foo > args.threshold:
                 chunks.append(chunk)
                 print(len(chunks), end='\r')
             else:
                 if chunks:
                     print()
-                if len(chunks) > nchunk:
+                if len(chunks) >= nchunk:
                     series += chunks
-                    print('!!!', len(series))
-                    if len(series) > nserie:
+                    print('!!!', len(series), nserie)
+                    if len(series) >= nserie:
                         return series
                 chunks = []
         plt.pause(1e-3)
@@ -66,7 +69,7 @@ notes = {}
 for note in range(args.nnote):
     print('press RETURN to record note %d' % (note + 1))
     #input()
-    series = get_series(args.ntraining)
+    series = get_series(args.ntraining, args.nchunk)
     notes[note] = series
 
 print('saving', args.output)
