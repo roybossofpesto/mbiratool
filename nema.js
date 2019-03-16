f7ddf146ae5fbb4c60e5d9d30f3e74a3d0a36534
Pierre Gueth <pierre.gueth@gmail.com>
2019-03-16 23:51:19 +0100

    3/4 finished

--- nema.js
+++ nema.js
@@ -61,16 +61,16 @@ const nema_full = (aa, bb, cc, index) => { // Nemamoussassa left hand & right ha
         create_note(aa, 0, 5),   create_note(aa, 4, 5),
         create_note(aa, 0, 4),   create_note(aa, 0, 6),
         create_note(bb+1, 0, 5), create_note(bb+1, 2, 5),
-        create_note(bb+1, 0, 4), create_note(bb+1, 4, 5),
+        create_note(bb+1, 0, 4), create_note(bb+1, 4, 6),
         create_note(cc+1, 4, 5), create_note(cc+1, 0, 5),
         create_note(cc+1, 0, 4), create_note(cc+1, 3, 6),
         ///////
         create_note(aa+1, 0, 5), create_note(aa+1, 4, 5),
-        create_note(aa+1, 0, 4), create_note(aa+1, 0, 6),
-        create_note(bb+1, 0, 5), create_note(bb+1, 2, 5),
-        create_note(bb+1, 0, 4), create_note(bb+1, 4, 5),
-        create_note(cc+1, 4, 5), create_note(cc+1, 0, 5),
-        create_note(cc+1, 0, 4), create_note(cc+1, 2, 6),
+        // create_note(aa+1, 0, 4), create_note(aa+1, 0, 6),
+        // create_note(bb+1, 0, 5), create_note(bb+1, 2, 5),
+        // create_note(bb+1, 0, 4), create_note(bb+1, 4, 5),
+        // create_note(cc+1, 4, 5), create_note(cc+1, 0, 5),
+        // create_note(cc+1, 0, 4), create_note(cc+1, 2, 6),
         ///////
         // { note: 'A5', chord: 5, delta: 0, octave: 5 },
     ];
