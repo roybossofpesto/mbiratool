aa5405effaeed9f4107013f2e8021ecba2cce473
Pierre Gueth <pierre.gueth@gmail.com>
2019-04-25 20:07:29 +0200

    right colors

--- Grid.js
+++ Grid.js
@@ -1,16 +1,14 @@
 "use strict";
 
-const root_key_colors = ["blue", "orange", "green", "purple", "teal", "red", "pink"];
+const root_key_colors = ["#1c96fe", "#feb831", "#aee742", "#b75ac4", "#15cdc2", "#fa2424", "#ff5986"].map(color => chroma(color));
 
 class NoteWidget {
 
     constructor(index) {
         this.__chord = null;
         this.__octave = 5;
+        this.__delta = -1;
         this.index = index;
-        this.delta = -1;
-        this.chord_color = "black";
-        this.root_key_color = "black";
 
         /*
                 const octave_elem = $.parseHTML(`<p><div class="ui mini icon vertical buttons">
@@ -32,23 +30,23 @@ class NoteWidget {
 
         const dual_button = $.parseHTML(`
         <div class="ui vertical fluid icon buttons">
-        <div class="ui icon top left pointing dropdown mini black button delta">
-            <div class="default text">&emptyset;</div>
-            <div class="menu">
-                <div class="item" data-value="-1">&emptyset;</div>
-                <div class="item" data-value="0">1st</div>
-                <div class="item" data-value="2">3rd</div>
-                <div class="item" data-value="4">5th</div>
+            <div class="ui icon top left pointing dropdown mini black button delta">
+                <div class="default text">&emptyset;</div>
+                <div class="menu">
+                    <div class="item" data-value="-1">&emptyset;</div>
+                    <div class="item" data-value="0">1st</div>
+                    <div class="item" data-value="2">3rd</div>
+                    <div class="item" data-value="4">5th</div>
+                </div>
             </div>
-        </div>
-        <div class="ui icon top left pointing dropdown mini black button octave">
-            <div class="default text">0</div>
-            <div class="menu">
-                <div class="item" data-value="6">+</div>
-                <div class="item" data-value="5">0</div>
-                <div class="item" data-value="4">-</div>
+            <div class="ui icon top left pointing dropdown mini black button octave">
+                <div class="default text">0</div>
+                <div class="menu">
+                    <div class="item" data-value="6">+</div>
+                    <div class="item" data-value="5">0</div>
+                    <div class="item" data-value="4">-</div>
+                </div>
             </div>
-        </div>
         </div>
         `);
 
@@ -64,19 +62,12 @@ class NoteWidget {
             },
         });
 
-        const delta_dropdown = $(dual_button).find('.ui.dropdown.delta');
-        delta_dropdown.dropdown({
+        this.delta_dropdown = $(dual_button).find('.ui.dropdown.delta');
+        this.delta_dropdown.dropdown({
             on: 'hover',
             duration: 0,
             onChange: function() {
-                const current = $(this);
-                const vv = parseInt(current.dropdown('get value'));
-
-                delta_dropdown.removeClass('active');
-                current.addClass('active');
-
-                self.delta = vv;
-
+                self.__delta = parseInt($(this).dropdown('get value'));
                 self.update();
             },
         });
@@ -92,22 +83,23 @@ class NoteWidget {
         this.elem = elem;
     }
 
+    get delta() {
+        return this.__delta;
+    }
+
+    set delta(value) {
+        this.__delta = value;
+        this.delta_dropdown.dropdown('set selected', value);
+        this.update();
+    }
+
     get octave() {
         return this.__octave;
     }
 
     set octave(value) {
         this.__octave = value;
-        // console.log(this.octave_dropdown, this.octave_dropdown.dropdown('get value'));
         this.octave_dropdown.dropdown('set selected', value);
-    }
-
-    get note() {
-        return this.delta < 0 ? null : create_note(this.chord, this.delta, this.octave);
-    }
-
-    set chord(root_key) {
-        this.__chord = root_key % 7;
         this.update();
     }
 
@@ -115,20 +107,20 @@ class NoteWidget {
         return this.__chord;
     }
 
+    set chord(root_key) {
+        this.__chord = root_key % 7;
+        this.update();
+    }
+
+    get note() {
+        return this.delta < 0 ? null : create_note(this.chord, this.delta, this.octave);
+    }
+
     update() {
         // console.log('NoteWidget', 'update', this.delta, this.octave, this.index, this.note);
         const root_color = root_key_colors[this.__chord];
-
-        const delta_dropdown = this.elem.find('.ui.dropdown.delta');
-
-        delta_dropdown.removeClass(this.chord_color);
-        this.chord_color = this.note == null ? "black" : root_color;
-        delta_dropdown.addClass(this.chord_color);
-
-        this.octave_dropdown.removeClass(this.root_key_color);
-        this.root_key_color = root_color;
-        this.octave_dropdown.addClass(this.root_key_color);
-
+        this.delta_dropdown.css('background-color', this.note == null ? "black" : root_color)
+        this.octave_dropdown.css('background-color', root_color);
         if (this.onUpdate) this.onUpdate();
     }
 }
