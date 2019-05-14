0e5872cafe525e322b75f085dd644dbb7061518c
roybossofpesto <andreduracell@gmail.com>
2019-05-14 04:19:34 +0200

    moved buzzer button && next_level

--- MbiraInstrument.js
+++ MbiraInstrument.js
@@ -28,8 +28,9 @@ class MbiraInstrument {
                 const next_note = widget.enabled && widget.note ? widget.note.note : null;
                 const next_pitch = next_note ? Tone.Frequency(next_note) : null;
                 const next_velocity = next_pitch ? .5  - (next_pitch - 520) / 1750 : null;
-                // console.log('hhhhh', next_velocity)
-                if (next_pitch && next_velocity) this.mbira_synth.triggerAttackRelease(next_pitch, "1n", time, next_velocity);
+                const next_length = next_pitch > 520 ? "64n" : "1n";
+                console.log('hhhhh', next_velocity, next_length);
+                if (next_pitch && next_velocity && next_length) this.mbira_synth.triggerAttackRelease(next_pitch, next_length, time, next_velocity);
             }
             Tone.Draw.schedule(() => widget.ping(), time);
         }, this.grid.widgets, "8t").start();
