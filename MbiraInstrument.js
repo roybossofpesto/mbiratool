4d33d9443e9cae3e4b80f222bd6531fede963fdc
roybossofpesto <andreduracell@gmail.com>
2019-05-14 04:54:38 +0200

    noise tuning

--- MbiraInstrument.js
+++ MbiraInstrument.js
@@ -28,8 +28,8 @@ class MbiraInstrument {
                 const next_note = widget.enabled && widget.note ? widget.note.note : null;
                 const next_pitch = next_note ? Tone.Frequency(next_note) : null;
                 const next_velocity = next_pitch ? .5  - (next_pitch - 520) / 1750 : null;
-                const next_length = next_pitch > 520 ? "64n" : "1n";
-                console.log('hhhhh', next_velocity, next_length);
+                const next_length = next_pitch ? (1500 - next_pitch)/1500 : null;
+                // console.log('hhhhh', next_velocity, next_length);
                 if (next_pitch && next_velocity && next_length) this.mbira_synth.triggerAttackRelease(next_pitch, next_length, time, next_velocity);
             }
             Tone.Draw.schedule(() => widget.ping(), time);
