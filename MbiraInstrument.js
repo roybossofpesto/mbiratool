c39df377cc1e4a89adfe3b10705f5f1f1805ffa1
Pierre Gueth <pierre.gueth@gmail.com>
2019-05-27 23:53:48 +0200

    added harmonics

--- MbiraInstrument.js
+++ MbiraInstrument.js
@@ -8,8 +8,8 @@ class MbiraInstrument {
         this.grid = new GridWidget(storage);
 
         // Mbira synth
-        this.mbira_synth = new Tone.PolySynth(24, Tone.Synth).connect(effects.auto_panner);
-        this.mbira_synth.set({
+        this.__mbira_synth = new Tone.PolySynth(24, Tone.Synth).connect(effects.auto_panner);
+        this.__mbira_synth.set({
             envelope: {
                 attack: 1e-2,
                 attackCurve: "exponential",
@@ -19,9 +19,31 @@ class MbiraInstrument {
                 release: .25,
                 releaseCurve: "ripple",
             },
+            oscillator: {
+                type: "sine",
+            },
             volume: -20,
         });
-        console.info("mbira_synth", this.mbira_synth.get());
+        console.info("mbira_synth", this.__mbira_synth.get());
+
+        // Harmonic synth
+        this.__harmonic_synth = new Tone.PolySynth(24, Tone.Synth).connect(effects.auto_panner);
+        this.__harmonic_synth.set({
+            envelope: {
+                attack: 1e-2,
+                attackCurve: "exponential",
+                decay: 1.,
+                decayCurve: "exponential",
+                sustain: 0,
+                release: .25,
+                releaseCurve: "ripple",
+            },
+            oscillator: {
+                type: "sine",
+            },
+            volume: -50,
+        });
+        console.info("harmonic_synth", this.__harmonic_synth.get());
 
         // Mbira loop
         const mbira_loop = new Tone.Pattern((time, widget) => {
@@ -32,7 +54,10 @@ class MbiraInstrument {
                 const next_velocity = next_pitch ? Math.max(.2, Math.min(1., .5 - (next_pitch - 520) / 1750 + .5 * (Math.random() - .5))) : null;
                 const next_length = next_pitch ? Math.max(.02, Math.min(.5, .5 - (next_pitch - 600) / 1200)) : null;
                 // console.log('hhhhh', next_pitch, next_velocity, next_length, this.transpose);
-                if (next_pitch && next_velocity && next_length) this.mbira_synth.triggerAttackRelease(next_pitch, next_length, time, next_velocity);
+                if (next_pitch && next_velocity && next_length) {
+                    this.__mbira_synth.triggerAttackRelease(next_pitch, next_length, time, next_velocity);
+                    this.__harmonic_synth.triggerAttackRelease(next_pitch.transpose(28), next_length, time, next_velocity);
+                }
             }
             Tone.Draw.schedule(() => widget.ping(), time);
         }, this.grid.widgets, "up");
@@ -43,7 +68,12 @@ class MbiraInstrument {
     }
 
     set decay(value) {
-        this.mbira_synth.set({
+        this.__mbira_synth.set({
+            envelope: {
+                decay: value / 1000.,
+            },
+        });
+        this.__harmonic_synth.set({
             envelope: {
                 decay: value / 1000.,
             },
